4097a48d992339a9fc41fb501d10fb76a4815c02
cesardpaz <cesardpaz@gmail.com>
2021-09-22 19:45:14 -0500

    Add classes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const formulario   = document.querySelector('#agregar-gasto');
 const gastoListado = document.querySelector('#gastos ul');
 
 
-
 //eventos
 eventListeners();
 function eventListeners(){
@@ -11,16 +10,36 @@ function eventListeners(){
 }
 
 
-//clases
+//clases 
+class Presupuesto {
 
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante    = Number(presupuesto);
+        this.gastos      = [];
+    }
 
+}
+
+//No require constructor
+class UI {
+
+}
+
+const ui = new UI();
+
+let presupuesto;
 
 
 //funciones
 function preguntarPresupuesto(){
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
+    
     //if user put blank text or click on cancel or user add letter or negative number
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
         window.location.reload();
     }
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+
 }
